f2311bb5bb04daee6cb4b3e07aa4c9cdc12d88d1
bongdaring <chlwlsdud258@gmail.com>
2023-10-25 09:48:09 +0900

    restful #2

--- ch08/src/main/java/ch08/controller/api/ApiRestController.java
+++ ch08/src/main/java/ch08/controller/api/ApiRestController.java
@@ -1,9 +1,19 @@
 package ch08.controller.api;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import ch08.dto.JsonResult;
 import ch08.vo.UserVo;
@@ -21,4 +31,33 @@ public class ApiRestController {
 		return JsonResult.success(vo);
 	}
 
-}
+	@GetMapping
+	public JsonResult read() {
+		List<UserVo> list = new ArrayList<>();
+		
+		UserVo vo1 = new UserVo();
+		vo1.setNo(1L);
+		vo1.setName("둘리");
+		vo1.setEmail("dooly@gmail.com");
+		list.add(vo1);
+		
+		UserVo vo2 = new UserVo();
+		vo1.setNo(2L);
+		vo1.setName("마이콜");
+		vo1.setEmail("michol@gmail.com");
+		list.add(vo2);
+		
+		return JsonResult.success(list);
+	}
+	
+	
+	@PutMapping("/{no}")
+	public JsonResult update(@PathVariable("no") Long no, @RequestBody UserVo vo) {
+		
+		vo.setNo(no);
+		vo.setPassword("");
+		
+		return JsonResult.success(vo);
+	}
+		
+	}
