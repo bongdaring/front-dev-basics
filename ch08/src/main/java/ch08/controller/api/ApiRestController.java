d52153005d0a92d4a3742d09ee00f5f96acb2739
bongdaring <chlwlsdud258@gmail.com>
2023-10-25 10:15:25 +0900

    rest #finish

--- ch08/src/main/java/ch08/controller/api/ApiRestController.java
+++ ch08/src/main/java/ch08/controller/api/ApiRestController.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -58,6 +59,15 @@ public class ApiRestController {
 		vo.setPassword("");
 		
 		return JsonResult.success(vo);
+	}
+	
+	@DeleteMapping("/{no}")
+	public JsonResult delete(
+			@PathVariable("no") Long no, 
+			@RequestParam(value="password", required = true, defaultValue = "") String password) {
+		
+		System.out.println("password" + password);
+		return JsonResult.success(no);
 	}
 		
 	}
